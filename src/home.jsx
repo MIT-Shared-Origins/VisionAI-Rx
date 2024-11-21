05c7d780fe95fd3c6b2af6ff8f8d121b3ea003e5
Gavin Weng <wengh7930@gmail.com>
2024-11-21 23:51:11 +1300

    add tailwindcss

---
+++ src/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+function Home() {
+  return (
+    <div>
+      <Button>Click me to Subscribe</Button>
+      dgaskhgdhaksg
+    </div>
+  )
+}
+
+export default Home
