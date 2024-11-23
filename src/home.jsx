0224518bf6a37328fcd544b4cfd74bab2766a119
Gavin Weng <wengh7930@gmail.com>
2024-11-24 01:20:00 +1300

    Add Hero section

--- src/home.jsx
+++ src/home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Button } from './components/ui/button'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function Home() {
   return (
     <div>
       {/* Header */}
       <Header/>
-      
+      {/* Hero */}
+      <Hero/>
     </div>
   )
 }
