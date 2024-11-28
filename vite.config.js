3277d8ae28dca707285aceeecd16ebfb6e13ca52
Gavin Weng <wengh7930@gmail.com>
2024-11-29 01:00:45 +1300

    update Hero section and cors proxy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    '/Production': {
+      target: 'https://feeb6yrl7e.execute-api.ap-southeast-2.amazonaws.com',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/Production/, '/Production'),
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
